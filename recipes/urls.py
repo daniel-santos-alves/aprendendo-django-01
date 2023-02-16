e11ad91a122e88f1ee3ec300b28c366181060858
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-16 20:09:53 -0300

    criando as primeiras rotas do app recipes

---
+++ recipes/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from recipes.views import contato, home, sobre
+
+urlpatterns = [
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
