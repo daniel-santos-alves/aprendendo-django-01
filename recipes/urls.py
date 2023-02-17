67cdd178af67f9d964ddee8401481cea80bdb9e4
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-16 22:16:50 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,10 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
