7a034b63c0dd8d504d0b3e72c112b4911ebc6614
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-20 17:00:41 -0300

    aplicando app_name nas URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipes, name="recipe"),
 
 ]
