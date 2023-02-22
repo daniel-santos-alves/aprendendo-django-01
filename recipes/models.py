2aa51c133bf5fb268b6d4aa5684cee0f83ca9093
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-22 10:17:42 -0300

    executando as migrações do models (padrão e Recipe)

--- recipes/models.py
+++ recipes/models.py
@@ -13,7 +13,7 @@ class Recipe(models.Model):
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.CharField(65)
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
