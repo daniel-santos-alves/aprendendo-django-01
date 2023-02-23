747f2140a744d41fe6e2b2e53780ad61e333bed3
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-23 16:31:24 -0300

    corrigindo bugs no template

--- recipes/models.py
+++ recipes/models.py
@@ -27,7 +27,8 @@ class Recipe(models.Model):
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default=""
     )
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
