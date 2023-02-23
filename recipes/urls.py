a20d826e84d9923c40e357f7067fe8ce9f72da98
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-23 15:43:12 -0300

    configurando o uso dos dados do BD local nos templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,3 @@
-
-from django.contrib import admin
 from django.urls import path
 
 from . import views
@@ -8,6 +6,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/<int:id>/', views.recipes, name="recipe"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
+    path('recipes/<int:id>/', views.recipes, name='recipe'),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,27 @@
 
 from django.shortcuts import render
 
-from utils.recipes.factory import make_recipe
+# from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
 def recipes(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipes-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
