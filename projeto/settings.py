88dd3ba523b90c762d06a23c0f4cf4549984bd2b
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-23 10:02:23 -0300

    adicionando Recipe no admin e configurando a pasta de uploads das imagens

--- projeto/settings.py
+++ projeto/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "base_static",
+    BASE_DIR / 'base_static',
 ]
 STATIC_ROOT = BASE_DIR / 'static'
+
+# arquivos enviados pelo usuário (upload)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,3 +8,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
