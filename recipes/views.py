2d689558b8b338771405660dbb93efc215a66f01
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-02-25 09:46:03 -0300

    configurando page not found

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 # from utils.recipes.factory import make_recipe
 from .models import Recipe
@@ -15,12 +15,16 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True
-    ).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
