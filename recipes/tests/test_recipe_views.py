44288e3c635dbea04123219a0947a406158c7c82
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-03-03 15:34:47 -0300

    adicionando teste page detail

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -67,3 +67,14 @@ class RecipeViewsTest(RecipeTestBase):
             reverse('recipes:recipe', kwargs={'id': 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_recipes(self):
+        needed_title = 'This is a detail page - It load one recipe'
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse(
+            'recipes:recipe', kwargs={'id': 1})
+        )
+
+        content = response.content.decode('utf-8')
+        self.assertIn(needed_title, content)
