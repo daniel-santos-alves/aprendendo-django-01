0e3564a5591b5dc4a91a1a03fa07970afc23f8d3
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-03-08 19:54:20 -0300

    refatorando os testes recipe_views

---
+++ recipes/tests/test_recipe_search_view.py
@@ -0,0 +1,30 @@
+from django.urls import resolve, reverse
+
+from recipes import views
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeSearchViewTest(RecipeTestBase):
+    def test_recipe_search_uses_correct_view_function(self):
+        url = reverse('recipes:search')
+        resolved = resolve(url)
+        self.assertIs(resolved.func, views.search)
+
+    def test_recipe_search_loads_correct_template(self):
+        url = reverse('recipes:search') + '?q=teste'
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=Teste'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;Teste&quot;',
+            response.content.decode('utf-8')
+        )
