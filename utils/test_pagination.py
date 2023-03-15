a9655a976373571edb9f3c46aefa38f6f5cef26f
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-03-14 21:20:55 -0300

    adicionando pagination

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -7,8 +7,43 @@ class paginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
-            qty_paginas=4,
+            qty_pages=4,
             current_page=1,
         )
 
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        # current page = 1 - qty page = 2 - middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # current page = 2 - qty page = 2 - middle page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # current page = 3 - qty page = 2 - middle page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        # current page = 4 - qty page = 2 - middle page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
