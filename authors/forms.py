0988411dc28e2f5a298b15f8dc9c80156b0bd59d
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-03-24 21:46:00 -0300

    configurando o forms - labels, help_texts e widgets

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,31 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid.'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+                'class': 'input text-input',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            }),
+        }
