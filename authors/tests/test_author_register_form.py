f0fd0bd75128c899501a758cd837a7116c2bf7f5
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-03-30 21:51:17 -0300

    adiconando teste de integração view, form e template

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,10 +1,13 @@
-from django.test import TestCase
+from unittest import TestCase
+
+from django.test import TestCase as DjangoTesteCase
+from django.urls import reverse
 from parameterized import parameterized
 
 from authors.forms import RegisterForm
 
 
-class AuthorRegisterFormUniTest(TestCase):
+class uthorRegisterFoArmUniTest(TestCase):
     @parameterized.expand([
         ('username', 'Your username'),
         ('email', 'Your e-mail'),
@@ -46,3 +49,26 @@ class AuthorRegisterFormUniTest(TestCase):
         form = RegisterForm()
         current_label = form[field].field.label
         self.assertEqual(current_label, needed)
+
+
+class uthorRegisterFoArmIntegrationTest(DjangoTesteCase):
+    def setUp(self, *args, **kwargs) -> None:
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@anyemail.com',
+            'password': 'Str0ngP@ssword1',
+            'password2': 'Str0ngP@ssword1',
+        }
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'This field must not be empty'),
+        ('password', 'Password must not be empty'),
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
