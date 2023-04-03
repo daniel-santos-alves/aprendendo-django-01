48774e94bc52138e21b2e23ddc890d569e422146
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-04-03 10:33:51 -0300

    corrigindo o campo de e-mail único

--- authors/forms.py
+++ authors/forms.py
@@ -102,6 +102,17 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalisd',
+            )
+
+        return email
+
     def clean(self):
         cleaned_data = super().clean()
 
