4d8a2b6ac706be7df82556382f8db280c3ce5c9d
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-04-10 23:23:28 -0300

    organizando os teste funcionais

---
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -0,0 +1,10 @@
+from base import RecipeBaseFunctionalTest
+from selenium.webdriver.common.by import By
+
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here', body.text)
