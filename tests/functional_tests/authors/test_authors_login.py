5cb2ba00d58ca236440bf996349275ca9178a707
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-04-26 20:35:07 -0300

    testando o formulário de login

---
+++ tests/functional_tests/authors/test_authors_login.py
@@ -0,0 +1,36 @@
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from selenium.webdriver.common.by import By
+
+from .base import AuthorsBaseTest
+
+
+@pytest.mark.functional_test
+class AuthorsLoginTest(AuthorsBaseTest):
+    def test_user_valid_data_can_login_sucessfully(self):
+        string_password = "pass123"
+        user = User.objects.create_superuser(
+            username="my_user", password=string_password
+        )  # noqa E501
+
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse("authors:login"))
+
+        # Usuário ver o formulário
+        form = self.browser.find_element(By.CLASS_NAME, "main-form")
+        username_field = self.get_by_placeholder(form, "Type your username")
+        password_field = self.get_by_placeholder(form, "Type your password")
+
+        # Usuário digita usuário e senha
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Usuário ver a mensagem de login com sucesso e seu nome
+        self.assertIn(
+            f"Your are logged in with {user.username}.",
+            self.browser.find_element(By.TAG_NAME, "body").text,
+        )
