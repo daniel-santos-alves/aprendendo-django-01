462492fd45f64b792f6846114dbc075ae886b5fa
Daniel Santos Alves <danielsantosalves672@gmail.com>
2023-05-03 20:31:03 -0300

    ajustando o admin do Django para melhorar a visualização das recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -12,4 +12,30 @@ admin.site.register(Category, CategoryAdmin)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "title",
+        "created_at",
+        "is_published",
+        "author",
+    )
+    list_display_links = (
+        "title",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "title",
+        "description",
+        "slug",
+        "preparation_steps",
+    )
+    list_filter = (
+        "category",
+        "author",
+        "is_published",
+        "preparation_steps_is_html",
+    )
+    list_per_page = 20
+    list_editable = ("is_published",)
+    ordering = ("-id",)
